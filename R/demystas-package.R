ef2bec28f8c28d91b6212baedd66f871f68aefea
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2018-09-22 16:30:08 +0200

    alias update

--- R/demystas-package.R
+++ R/demystas-package.R
@@ -3,12 +3,12 @@
 #' There are a myriad of daily tasks which seem simple, but yet hide complex computational requirements.
 #' These are the kind of challenges that we encounter that are not entirely part of our main objective,
 #' but need to be overcome nonetheless. These include conducting complicated string searches, querying spatial
-#' information and sometimes just organizing messy data. 
+#' information and sometimes just organizing messy data.
 #'
 #' This package aims to offer useful functions for data analysts
 #' which might help in making these "on-the-fly" tasks lighter and more automated.
 #'
 #' @name demystas-package
-#' @aliases demystas-package demystas
+#' @aliases demystas
 #' @docType package
 NULL
