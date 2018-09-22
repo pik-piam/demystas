18ff8d2e7ee969e76e8e8d9e817633b0e745c623
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2018-09-22 13:59:13 +0200

    description changes

---
+++ R/demystas-package.R
@@ -0,0 +1,14 @@
+#' Demystifying Automation on Simple Tasks (demystas)
+#'
+#' There are a myriad of daily tasks which seem simple, but yet hide complex computational requirements.
+#' These are the kind of challenges that we encounter that are not entirely part of our main objective,
+#' but need to be overcome nonetheless. These include conducting complicated string searches, querying spatial
+#' information and sometimes just organizing messy data. 
+#'
+#' This package aims to offer useful functions for data analysts
+#' which might help in making these "on-the-fly" tasks lighter and more automated.
+#'
+#' @name demystas-package
+#' @aliases demystas-package demystas
+#' @docType package
+NULL
